c4eed48c8425a905ea9a181e261873c2c2ab4b0e
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-08-24 18:35:51 -0500

    feat(country): fetch country data and log to console

--- app/routes/country.tsx
+++ app/routes/country.tsx
@@ -1,4 +1,16 @@
-const country = () => {
+import type { Route } from "./+types/country";
+
+export async function clientLoader({params}: Route.LoaderArgs) {
+  const countryName = params.countryName
+  const res = await fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true
+`)
+    const data = await res.json();
+    return data;
+}
+
+
+const country = ({loaderData}: Route.ComponentProps) => {
+  console.log(loaderData)
   return (
     <div>country</div>
   )
