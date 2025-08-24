abe86e4f21edae991aedfa2543081c21d03e236c
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-08-24 17:51:41 -0500

    feat(api): fetch and render data from RestCountries with types

--- app/routes/countries.tsx
+++ app/routes/countries.tsx
@@ -1,7 +1,45 @@
-const countries = () => {
-  return (
-    <div>countries</div>
-  )
+import type { Route } from "./+types/countries";
+import type { Country } from '../types/index';
+import { Link } from "react-router";
+
+export async function clientLoader(): Promise<Country[]> {
+    const res = await fetch(
+        "https://restcountries.com/v3.1/independent?status=true&fields=name,continents,population"
+    );
+    const data: Country[] = await res.json();
+    return data;
 }
 
-export default countries
+
+export function HydrateFallback() {
+    return <p>Loading...</p>;
+}
+
+
+
+const Countries = ({ loaderData }: Route.ComponentProps) => {
+
+
+    if (!loaderData) {
+        return <p>No hay datos</p>;
+    }
+
+    const countries = loaderData as Country[];
+
+    return (
+        <div>
+            <ul>
+                {countries.map((country: Country) => (
+                    <li key={country.name.official}>
+                        <Link to={`/countries/${country.name.common}`} >{country.name.common}</Link>
+                        <div>
+                            Region: {country.continents[0]} | Population: {country.population}
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Countries
---
+++ app/types/index.ts
@@ -0,0 +1,25 @@
+export interface Country {
+    name:       Name;
+    population: number;
+    continents: Continent[];
+}
+
+export enum Continent {
+    Africa = "Africa",
+    Asia = "Asia",
+    Europe = "Europe",
+    NorthAmerica = "North America",
+    Oceania = "Oceania",
+    SouthAmerica = "South America",
+}
+
+export interface Name {
+    common:     string;
+    official:   string;
+    nativeName: { [key: string]: NativeName };
+}
+
+export interface NativeName {
+    official: string;
+    common:   string;
+}
